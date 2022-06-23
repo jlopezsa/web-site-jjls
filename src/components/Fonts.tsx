92f48636924372e6a754962fa17ae9b92515d60c
Julian Lopez <jlopezsa@gmail.com>
2022-06-23 00:34:10 -0500

    feat: create styles in menu of the nav-bar

--- src/components/Fonts.tsx
+++ src/components/Fonts.tsx
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
-import { colors } from '../styles/globalStyles';
+import { colors, fontsDesktop } from '../styles/globalStyles';
 
 export const FNT = {
   H3: styled.h3`
-    font-size: 35.2px;
+    font-size: ${fontsDesktop.headline3Desktop};
     text-align: center;
     color: ${colors.wc1};
     margin-top: 50px;
     margin-bottom: 0px;
     `,
   H5: styled.h5`
-    font-size: 22.5px;
+    font-size: ${fontsDesktop.headline5Desktop};
     text-align: center;
     color: ${colors.wc1};
     margin-top: 8px;
