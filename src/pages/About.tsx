0ecefb75714debd8b9b8370b3e6c5ad9c8410170
LopezPin <lopezpin@MacBook-Air-de-LopezPin.local>
2024-03-02 12:54:27 -0500

    ADD: Messages about my general description

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,6 +6,7 @@ import StudyCard from "../components/StudyCard";
 import { colors } from "../styles/globalStyles";
 import { StudyMessage } from "./messages/study-message";
 import { GlobalCardMessage } from "./messages/global-card-message";
+import { GeneralInfo } from "./messages/general-info";
 
 const swDeveloper = "software-developer-640.jpg";
 const research = "science_640.jpg";
@@ -38,24 +39,26 @@ const Abt = {
   `,
   lastColumn: styled.div`
     background-color: ${colors.bc1};
+    widh: 100%;
   `,
+  menu: styled.div``,
 };
 
 const About = () => {
   return (
     <Abt.container>
-      <div>
+      <Abt.menu>
         <NavBar />
-      </div>
+      </Abt.menu>
       <Abt.colMedium>
         <SingleInfo
-          title="Summary"
-          description="Hi there 👋, I am a Telecommunications engineer and Full-Stack developer using the MERN Stack and, also, familiarized with software development using agile methodologies such as SCRUM. Daily I use: React, Javascript, CSS, Styled-components, Nodejs. I have more than 13 years of experience in telecommunication system research and, among them, 3 years as professor in systems and telecommunications engineering, in Brasilian and Colombian universities."
+          title={GeneralInfo.spa.aboutMe.title}
+          description={GeneralInfo.spa.aboutMe.description}
         />
         <Abt.hr />
         <SingleInfo
-          title="Experience"
-          description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos harum eaque odit voluptas dolores exercitationem qui atque! Facilis officiis voluptatibus rem! Quia, at dignissimos repellat alias tenetur explicabo aperiam. Non. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis autem ea velit. Architecto iure cumque maxime, reprehenderit minima ut, accusantium, ipsum incidunt repudiandae nesciunt sint libero aliquid! Architecto perspiciatis ratione quas voluptatibus sit et asperiores nulla voluptas aut dolorem dolore repellat praesentium in, ipsam eius consequatur. Sapiente eum molestias"
+          title={GeneralInfo.spa.experience.title}
+          description={GeneralInfo.spa.experience.description}
         />
         <Abt.setCard>
           <GlobalCard
---
+++ src/pages/messages/general-info.ts
@@ -0,0 +1,29 @@
+const eng = {
+  aboutMe: {
+    title: "Summary",
+    description:
+      "Hi there 👋, I am a Telecommunications engineer and Full-Stack developer using the MERN Stack and, also, familiarized with software development using agile methodologies such as SCRUM. Daily I use: React, Javascript, CSS, Styled-components, Nodejs. I have more than 13 years of experience in telecommunication system research and, among them, 3 years as professor in systems and telecommunications engineering, in Brasilian and Colombian universities.",
+  },
+  experience: {
+    title: "Experience",
+    description: "asdjsalkd jlksadj lksajdsalk jdlk",
+  },
+};
+
+const spa = {
+  aboutMe: {
+    title: "Acerca de mí",
+    description:
+      "Soy Julián López, ingeniero en Telecomunicaciones y desarrollador Full-Stack utilizando las tecnologías MongoDB, Express, React y Node.js (MERN Stack). Familiarizado con el desarrollo de software usando metodologías ágiles como SCRUM.",
+  },
+  experience: {
+    title: "Experiencia",
+    description:
+      "Cuento con más de 13 años de experiencia en el área de investigación de sistemas de telecomunicaciones y, entre ellos, 3 años en docencia en ingeniería de sistemas e ingeniería en telecomunicaciones. Gran parte de esta experiencia y estudios los viví en Brasil durante 12 años en las ciudades de Florianópolis y Araranguá.",
+  },
+};
+
+export const GeneralInfo = {
+  eng,
+  spa,
+};
